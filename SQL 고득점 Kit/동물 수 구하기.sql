18e748d8650dc4bd7f8bd14d5f8f22a262e627c9
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-26 00:50:19 +0900

    Create 동물 수 구하기.sql

---
+++ SQL 고득점 Kit/동물 수 구하기.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59406?language=oracle
+-- 코드를 입력하세요
+SELECT COUNT(*) AS count
+FROM ANIMAL_INS
+;
+
+-- count(*) : null값을 포함해 존재하는 모든 행 카운트
+-- count(컬럼명) : null값을 제외하고 카운트
