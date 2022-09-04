2bd184bb0de97638246dd085fec771ee128ff113
jinnkimm7 <jinnkimm7@gmail.com>
2022-09-04 18:02:25 +0900

    Create 이름에 el이 들어가는 동물 찾기.sql

---
+++ SQL 고득점 Kit/이름에 el이 들어가는 동물 찾기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59047
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'Dog' AND (NAME LIKE '%el%' OR NAME LIKE '%El%')
+ORDER BY NAME
+;
